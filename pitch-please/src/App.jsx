262e349b827cf82632ea208063689108dcdcbff0
lileeluna <elliechan.nhs@gmail.com>
2026-06-17 12:47:11 -0700

    feat/basic router setup started

--- pitch-please/src/App.jsx
+++ pitch-please/src/App.jsx
@@ -1,11 +1,28 @@
-// import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Navbar from './pages/NavBar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Gallery from './pages/Gallery'
+import Members from './pages/Members'
+import Contact from './pages/Contact'
 
 function App() {
 
   return (
     <>
-      <h1>This is the new PP website! Work in Progress.</h1>
+        <BrowserRouter>
+            <Navbar />
+            <h1>This is the new PP website! Work in Progress.</h1>
+
+            <Routes>
+                <Route path = '/' element={<Home />} />
+                <Route path = '/about' element={<About />} />
+                <Route path = '/gallery' element={<Gallery />} />
+                <Route path = '/members' element={<Members />} />
+                <Route path = '/contact-us' element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
     </>
   )
 }

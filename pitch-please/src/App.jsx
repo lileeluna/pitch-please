a598d12405cd4a08435ae716c24cef98e0f629d9
lileeluna <elliechan.nhs@gmail.com>
2026-06-17 14:17:14 -0700

    fix/very minor eslint error

--- pitch-please/src/App.jsx
+++ pitch-please/src/App.jsx
@@ -1,28 +1,27 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import './App.css'
-import Navbar from './pages/NavBar'
-import Home from './pages/Home'
-import About from './pages/About'
-import Gallery from './pages/Gallery'
-import Members from './pages/Members'
-import Contact from './pages/Contact'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./pages/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import Members from "./pages/Members";
+import Contact from "./pages/Contact";
 
 function App() {
-
   return (
     <>
-        <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path = '/' element={<Home />} />
-                <Route path = '/about' element={<About />} />
-                <Route path = '/gallery' element={<Gallery />} />
-                <Route path = '/members' element={<Members />} />
-                <Route path = '/contact-us' element={<Contact />} />
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/contact-us" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

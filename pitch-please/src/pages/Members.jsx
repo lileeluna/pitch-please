8643e99720d1e1c86e3a7afe2ce19dcb7f4d2d6e
lileeluna <elliechan.nhs@gmail.com>
2026-07-18 16:41:58 -0700

    feat: added gallery page

--- pitch-please/src/pages/Members.jsx
+++ pitch-please/src/pages/Members.jsx
@@ -1,7 +1,113 @@
+import back from "../assets/members.jpg";
+import front from "../assets/members-transparent.png";
+import "../styles/Members.css";
+
 function Members() {
   return (
     <>
-      <p>MEMBERS</p>
+      <div className="container">
+        <img src={back} className="bg-img"></img>
+        <h1 className="member-title">Meet The Pitches!</h1>
+        <img src={front} className="front-img"></img>
+      </div>
+      <section className="members-grid">
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+        <div className="member-square">
+          <div className="member-img"></div>
+          <div className="member-name">First Last</div>
+          <div className="member-part">Vocal Part</div>
+          <div className="board-pos">Member</div>
+        </div>
+      </section>
     </>
   );
 }

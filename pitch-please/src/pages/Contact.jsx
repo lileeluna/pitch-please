44fc76cc43720ee62b25b8fb5e7761c24a52ba59
lileeluna <elliechan.nhs@gmail.com>
2026-07-18 14:36:17 -0700

    feat: contact page added

--- pitch-please/src/pages/Contact.jsx
+++ pitch-please/src/pages/Contact.jsx
@@ -1,9 +1,58 @@
+import image from "../assets/contact.jpg";
 import "../styles/Contact.css";
 
 function Contact() {
   return (
     <>
-      <p>CONTACT</p>
+      <div className="contact-container">
+        <img src={image} className="contact-image" />
+        <div className="contact-image-overlay"></div>
+        <div className="contact-text-overlay">
+          <h1 className="contact-title">CONTACT US</h1>
+        </div>
+        <section className="contact-content">
+          <div className="contact-left">
+            <div className="header-row">
+              <div className="header-label">Name</div>
+              <div className="header-label">Email</div>
+            </div>
+            <div className="input-row">
+              <input type="text" className="half-row" />
+              <input type="text" className="half-row" />
+            </div>
+            <div className="header-row">
+              <div className="header-label">Subject</div>
+            </div>
+            <div className="input-row">
+              <input type="text" className="full-row" />
+            </div>
+            <div className="header-row">
+              <div className="header-label">Message</div>
+            </div>
+            <div className="input-row">
+              <textarea className="full-row message-box"></textarea>
+            </div>
+            <div className="submit-row">
+              <div className="submit-btn">Submit</div>
+            </div>
+          </div>
+          <div className="contact-right">
+            <p>
+              Have any questions for us? Please feel free to reach out on our
+              Instagram or at{" "}
+              <a
+                href="mailto:pitchpleaseucla@gmail.com"
+                style={{ textDecoration: "underline" }}
+                className="email"
+              >
+                pitchpleaseucla@gmail.com
+              </a>
+              . If you have an event you'd be interested in having us perform
+              for, let us know!
+            </p>
+          </div>
+        </section>
+      </div>
     </>
   );
 }

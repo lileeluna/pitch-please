73075ff003ce296e906e1e1f6360e24f20be18bb
lileeluna <elliechan.nhs@gmail.com>
2026-09-18 11:31:55 -0700

    fix: backend url fix

--- pitch-please/vite.config.js
+++ pitch-please/vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://pitch-please-yo0l.onrender.com",
-      "/uploads": "https://pitch-please-yo0l.onrender.com",
-    },
-  },
 });
